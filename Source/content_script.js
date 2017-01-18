5b32ff26aa091ed80cd6452a737cca9772f71e92
Campion Fellin <campionfellin@gmail.com>
2017-01-18 13:58:20 -0800

    next

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,5 +1,14 @@
 var elements = document.getElementsByTagName('*');
 
+var phrases = [
+    "the big orange",
+    "the liar",
+    "the scumbag",
+    "the president elect",
+    "the bad businessman"
+]
+
+
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
@@ -8,11 +17,19 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/\bTrump\b/g, 'the big orange');
+            var replacedText = text.replace(/\bTrump\b/g, generateRandomPhrase());
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
+}
+
+function generateRandomPhrase() {
+    
+
+    var location = Math.floor(Math.random() * 5);
+
+    return phrases[location];
 }
